92b15a3d622295008f76589d3ae988b8e8a034dd
gustawx <haslO1234?>
2023-10-09 17:08:54 +0200

    feat(new_expense.dart): add date picker, title and amount fields to NewExpense widget refactor(new_expense.dart): replace static padding with dynamic padding for better UI responsiveness feat(new_expense.dart): add save and cancel buttons to NewExpense widget for better user interaction refactor(new_expense.dart): replace direct state manipulation with TextEditingController for better state management

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -10,17 +11,94 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+  final _titleCOntroller = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+
+    var firstDate = DateTime(now.year - 1, now.month, now.day);
+    var lastDate = now;
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: lastDate);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleCOntroller.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            // onChanged: _saveTitleInput,
+            controller: _titleCOntroller,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  // onChanged: _saveTitleInput,
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                      label: Text('Amount'), prefixText: '\$ '),
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_selectedDate == null
+                      ? 'No date selected'
+                      : formatter.format(_selectedDate!)),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(Icons.calendar_month),
+                  )
+                ],
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_amountController.text);
+                  },
+                  child: const Text('Save expense'))
+            ],
           ),
         ],
       ),
